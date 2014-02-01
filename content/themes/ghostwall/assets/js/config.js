d033fdca61a66384b36d1bc5ceeb043f37cd50aa
Francisco Calle <francisco.calle.moreno@gmail.com>
2014-02-01 04:01:07 +0100

    Site update. Theme configured.

--- content/themes/ghostwall/assets/js/config.js
+++ content/themes/ghostwall/assets/js/config.js
@@ -2,9 +2,9 @@
 /*  1. Global Variables Define
 /*================================================================*/
 var flickr_id='YOUR_FLICKR_ID_HERE';
-var instagram_accessToken='your-instagram-access-token'; // change [your-instagram-access-token] here
-var instagram_clientID='your-instagram-application-clientID'; // change [your-instagram-application-clientID] here
-var your_latitude   = 10.839776; // Your map latitude
-var your_longitude  = 106.647159; // Your map longitude
-var enable_tag_count= false;
+var instagram_accessToken='327400110f4e54a.80374ecf59df40b9bc30d78595f34bfd'; // change [your-instagram-access-token] here
+var instagram_clientID='0f4e54a1903546518301a874ad288e94'; // change [your-instagram-application-clientID] here
+var your_latitude   = 40.2970286; // Your map latitude
+var your_longitude  = -3.8312561; // Your map longitude
+var enable_tag_count= true;
 var rememberLastActiveTag=false;
